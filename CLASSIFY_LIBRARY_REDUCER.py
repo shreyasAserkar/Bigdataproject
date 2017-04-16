aeefca64566c4d2cef607bd492e3238585af6807
Mayur Nimba Beldar <mbeldar@okstate.edu>
2017-04-15 20:33:44 -0500

    Applied some filters to get cleaned output for analysis

--- CLASSIFY_LIBRARY_REDUCER.py
+++ CLASSIFY_LIBRARY_REDUCER.py
@@ -2,7 +2,7 @@
 import sys
 def CLASSIFY_LIBRARY_REDUCER():
     '''this method act as reducer and will get key value from mapper. It classifies each tweet and then assigns catagory to it\n
-    \nwhich is later used to classify each library.'''
+    which is later used to classify each library.'''
     cat1=[]
     cat2=[]
     cat3=[]
@@ -10,7 +10,16 @@ def CLASSIFY_LIBRARY_REDUCER():
     outPut1 = {}
     #set of classified data we got from paper. Based on this we will classify our own data.
     l = ["1:archives,archive,books,book,list,read,bestselling,articles,cookbook,benefit,photo ,cover,picture,history,cookbook,reads,info,news,check,collection,booklist,article,peek,edition,essay,essays,guide,document,documents,readers,resource,resources","2:readinglist,school,excited,encourage,happy,love,wisdom,enjoyed,parents,favourite,readeverywhere,learning,kids,favourite,blog,http,show,lovely,exciting,movie,movies,film,films,myths,ideas,popular,think,thinking,look,oscar,oscars","3:join,celebration,libraries,opening,program,party ,creative,story,questions,today,community,art,family,summer,spring,fun,celebrate,creative,club,check,questions,story,joined,meet,meeting,participate,communities,celebrating,enjoy,thanks,thank you,ready,open,congratulations,award,publishers,congrats","4:public,librarian,visit,staff,branch,exhibit,welcome,opening,event,program,food,library,friends,park,explore,events,webinar,hosted,host,hosting,activities,exhibitions,offers,offer,workshop,librarians,music,reminder,tomorrow,campaign,tonight ,day,update,week,newsletter,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday,subscribe,alert,working,upcoming,watch"]
-
+    l2 = ["u'Oregon State Library'",
+          "u'Hamilton PL'",
+          "u'Texas State Library'",
+          "u'AustinPublicLibrary'",
+          "u'Yale Library'",
+          "u'NY Public Library'",
+          "u'Beinecke Library'",
+          "u\"Washington's Library\"",
+          "u'OkStateLibrary'",
+          "u'Eugene Library'"]
     #this step will add each category to respective list
     for line in l:
         try:
@@ -54,6 +63,7 @@ def CLASSIFY_LIBRARY_REDUCER():
             if c1 is 0 and c2 is 0 and c3 is 0 and c4 is 0:
                 c2 = 99
             mapOfcat = {'c1': c1, 'c2': c2, 'c3': c3, 'c4': c4}
+
             # print '%s:::::%s' % (words[0].split('|')[2]+'|'+max(mapOfcat, key=mapOfcat.get), words[1].strip())
             if str(words[0].split('|')[2] + '|' + max(mapOfcat, key=mapOfcat.get)).strip() in outPut1:
                 outPut1[str(words[0].split('|')[2] + '|' + max(mapOfcat, key=mapOfcat.get)).strip()] = outPut1[str(
@@ -73,22 +83,24 @@ def CLASSIFY_LIBRARY_REDUCER():
 
 
     for k in outPut2:
-        mx = 0;
-        catg = 'c2';
-        for values in outPut2[k]:
 
-            v,CurrMx = values.split('|',1)
-            if int(CurrMx) > int(mx) :
-                mx = int(CurrMx)
-                catg = v
-        catg = catg.strip()
+        if k in l2:
+            mx = 0;
+            catg = 'c2';
+            for values in outPut2[k]:
 
-        if catg == 'c1':
-            print '%s\t%s' % (k, 'Literature Exhibit')  # append M so that reducer can differentiate data from two tables
-        if catg == 'c2':
-            print '%s\t%s' % (k, 'Engaging Topics')  # append M so that reducer can differentiate data from two tables
-        if catg == 'c3':
-            print '%s\t%s' % (k, 'Comminity Building')
-        if catg == 'c4':
-            print '%s\t%s' % (k, 'Library Showcasing')
+                v,CurrMx = values.split('|',1)
+                if int(CurrMx) > int(mx) :
+                    mx = int(CurrMx)
+                    catg = v
+            catg = catg.strip()
+
+            if catg == 'c1':
+                print '%s\t%s' % (k, 'Literature Exhibit')  # append M so that reducer can differentiate data from two tables
+            if catg == 'c2':
+                print '%s\t%s' % (k, 'Engaging Topics')  # append M so that reducer can differentiate data from two tables
+            if catg == 'c3':
+                print '%s\t%s' % (k, 'Comminity Building')
+            if catg == 'c4':
+                print '%s\t%s' % (k, 'Library Showcasing')
 if  __name__ =='__main__':CLASSIFY_LIBRARY_REDUCER()
